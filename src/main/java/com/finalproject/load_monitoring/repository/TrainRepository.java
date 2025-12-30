f4a05eb3a950eaa970c8fcda79773e98a80b4c0c
Omer Meler <Omer.Meler@s.afeka.ac.il>
2025-12-30 16:25:47 +0200

    Add 2 Query Methods

--- src/main/java/com/finalproject/load_monitoring/repository/TrainRepository.java
+++ src/main/java/com/finalproject/load_monitoring/repository/TrainRepository.java
@@ -3,5 +3,11 @@ package com.finalproject.load_monitoring.repository;
 import com.finalproject.load_monitoring.entity.Train;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TrainRepository extends JpaRepository<Train, Long> {
+
+    public List<Train> findAllByDepartureTimeBetween(LocalDateTime start, LocalDateTime end);
+    public List<Train> findAllByOriginStationAndDestinationStation(String originStation, String destinationStation);
 }
