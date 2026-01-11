445d873fe93a51abd25140de7766cd2eacbfee2f
Omer Meler <Omer.Meler@s.afeka.ac.il>
2026-01-11 14:51:26 +0200

    Change findByCarriageNumber to findByTrainTrainIdAndCarriageNumber. Add findByTrainTrainIdOrderByCarriageNumberAsc

--- src/main/java/com/finalproject/load_monitoring/repository/CarriageRepository.java
+++ src/main/java/com/finalproject/load_monitoring/repository/CarriageRepository.java
@@ -3,9 +3,11 @@ package com.finalproject.load_monitoring.repository;
 import com.finalproject.load_monitoring.entity.Carriage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CarriageRepository extends JpaRepository<Carriage, Long> {
 
-    public Optional<Carriage> findByCarriageNumber(int carriageNumber);
+    public Optional<Carriage> findByTrainTrainIdAndCarriageNumber(Long trainId, int carriageNumber);
+    public List<Carriage> findByTrainTrainIdOrderByCarriageNumberAsc(Long trainId);
 }
