ef1f846667eb3a43ce4ddd131d157b06e4703e80
Omer Meler <Omer.Meler@s.afeka.ac.il>
2026-01-11 14:52:28 +0200

    Add DTO to entity.

--- src/main/java/com/finalproject/load_monitoring/dto/DtoMapper.java
+++ src/main/java/com/finalproject/load_monitoring/dto/DtoMapper.java
@@ -2,6 +2,7 @@ package com.finalproject.load_monitoring.dto;
 
 import com.finalproject.load_monitoring.entity.Carriage;
 import com.finalproject.load_monitoring.entity.Train;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -16,4 +17,9 @@ public interface DtoMapper {
 
     @Mapping(source = "train.trainId", target = "trainId")
     CarriageDTO toCarriageDTO(Carriage carriage);
+
+    Train toEntity(TrainDTO trainDTO);
+
+    @InheritInverseConfiguration
+    Carriage toEntity(CarriageDTO carriageDTO);
 }
